f2b163207cc1562ea15847a79fef0fc7da489962
KTNza <fifaopoa540@gmail.com>
2026-05-25 22:42:37 +0700

    eiei

--- lib/Connect.dart
+++ lib/Connect.dart
@@ -64,7 +64,7 @@ class _ConnectPageState extends State<ConnectPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200], // พื้นหลังสีเทาอ่อน
+      backgroundColor: Colors.grey[200], // พื้นหลังสีเทาอ่อนอิอิ
       appBar: AppBar(
         automaticallyImplyLeading: false, // เอาปุ่ม back ออก
         backgroundColor: Colors.grey[200],
